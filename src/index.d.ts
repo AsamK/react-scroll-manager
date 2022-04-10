6d7ebefe03ec9b1bfce444a08c2b5e43c2db7847
Sebastian <asamk@gmx.de>
2022-04-10 12:26:59 +0200

    Fix typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,6 +5,7 @@ export interface ScrollManagerProps {
   history: History;
   sessionKey?: string;
   timeout?: number;
+  children: React.ReactNode;
 }
 
 export class ScrollManager extends React.Component<ScrollManagerProps> { }
@@ -13,6 +14,7 @@ export class WindowScroller extends React.Component { }
 
 export interface ElementScrollerProps {
   scrollKey: string;
+  children: React.ReactNode;
 }
 
 export class ElementScroller extends React.Component<ElementScrollerProps> { }
